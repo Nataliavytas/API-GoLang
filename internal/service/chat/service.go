00f1c40526437842d92e3eb64a70520b13e81e08
Natalia Vytas <nvytas@temperies.co>
2020-11-30 01:26:45 -0300

    CRUD working

--- internal/service/chat/service.go
+++ internal/service/chat/service.go
@@ -7,15 +7,18 @@ import (
 
 // Message ...
 type Message struct {
-	ID   int64
-	Text string
+	ID   int64  `json:"id"`
+	Text string `json:"message"`
 }
 
 //Service ...
 type Service interface {
 	AddMessage(Message) error
-	FindByID(int) *Message
+	DeleteMessage(int) error
+	FindByID(int) (*Message, error)
 	FindAll() []*Message
+	PostMessage(Message) error
+	UpdateMessage(id int, message Message) error
 }
 
 type service struct {
@@ -32,14 +35,56 @@ func (s service) AddMessage(m Message) error {
 	return nil
 }
 
-func (s service) FindByID(ID int) *Message {
-	return nil
-}
-
 func (s service) FindAll() []*Message {
 	var list []*Message
 	if err := s.db.Select(&list, "SELECT * FROM messages"); err != nil {
 		panic(err)
 	}
 	return list
+}
+
+func (s service) FindByID(ID int) (*Message, error) {
+	var message Message
+
+	sentence := `SELECT * FROM messages WHERE id=?;`
+	s.db.MustExec(sentence, ID)
+
+	err := s.db.QueryRowx(sentence, ID).StructScan(&message)
+
+	if err != nil {
+		return nil, err
+	}
+	return &message, nil
+}
+
+func (s service) PostMessage(message Message) error {
+	sentence := "INSERT INTO messages (text) VALUES (?)"
+	_, err := s.db.Exec(sentence, message.Text)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s service) DeleteMessage(id int) error {
+	sentence := `DELETE FROM messages WHERE id=?;`
+	_, err := s.db.Exec(sentence, id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s service) UpdateMessage(id int, message Message) error {
+	sentence := `UPDATE messages SET text = ? WHERE id=?;`
+	_, err := s.db.Exec(sentence, message.Text, id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/service/chat/transport.go
+++ internal/service/chat/transport.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -35,6 +36,27 @@ func makeEndpoints(s Service) []*endpoint {
 		path:     "/messages",
 		function: getAll(s),
 	})
+	list = append(list, &endpoint{
+		method:   "GET",
+		path:     "/message/:id",
+		function: getMessageByID(s),
+	})
+	list = append(list, &endpoint{
+		method:   "POST",
+		path:     "/message",
+		function: postMessage(s),
+	})
+	list = append(list, &endpoint{
+		method:   "DELETE",
+		path:     "/message/:id",
+		function: deleteMessage(s),
+	})
+	list = append(list, &endpoint{
+		method:   "PUT",
+		path:     "/message/:id",
+		function: putMessage(s),
+	})
+
 	return list
 }
 
@@ -46,9 +68,101 @@ func getAll(s Service) gin.HandlerFunc {
 	}
 }
 
+func deleteMessage(s Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"Error": "Identificador Invalido.",
+			})
+		}
+
+		posibleErr := s.DeleteMessage(id)
+		if posibleErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"Error": "Ocurrio un error. No se pudo eliminar el elemento.",
+			})
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"Elemento eliminadao": "El elemento se elimino correctamente.",
+		})
+	}
+}
+
+func getMessageByID(s Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		i, err := strconv.Atoi(id)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"Error": "Identificador Invalido.",
+			})
+		}
+		message, error := s.FindByID(i)
+		if error != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"Error": "No se encontro ningun elemento con dicho ID.",
+			})
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": *message,
+		})
+	}
+}
+
 //Register ...
 func (s httpService) Register(r *gin.Engine) {
 	for _, e := range s.endpoints {
 		r.Handle(e.method, e.path, e.function)
+	}
+}
+
+func postMessage(s Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var message Message
+		err := c.BindJSON(&message)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"Error": "Ocurrio un error, intente nuevamente.",
+			})
+		}
+		postErr := s.PostMessage(message)
+		if postErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"Error": "Ocurrio un error, intente nuevamente.",
+			})
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"messages": "Se inserto el elemento con exito",
+		})
+	}
+}
+
+func putMessage(s Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var message Message
+		err := c.BindJSON(&message)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"Error": "Ocurrio un error, intente nuevamente.",
+			})
+		}
+
+		id := c.Param("id")
+		i, err := strconv.Atoi(id)
+
+		updateErr := s.UpdateMessage(i, message)
+
+		if updateErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"Error": "Ocurrio un error, intente nuevamente.",
+			})
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"messages": "Se actualizo el elemento con exito",
+		})
 	}
 }
